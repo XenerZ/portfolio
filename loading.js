f26afcdfc82211c25637b2653500eaf0a1f273ff
Bartek Dabrowski <bartek.dab@poczta.fm>
2024-06-08 23:19:54 +0200

    as

--- loading.js
+++ loading.js
@@ -1,20 +1,26 @@
 window.addEventListener("load", () => {
     var load = document.querySelector(".onload");
+    var body = document.querySelector("body");
 
     if(load.classList.contains("load-anim")) {
         load.classList.remove("load-anim");
+        body.classList.remove("body-onload");
 
+        body.classList.toggle("body-onload");
         setTimeout(() => {
             load.classList.toggle("load-anim");
             setTimeout(() => {
                 load.style.display = "none";
+                body.classList.toggle("body-onload");
             }, 300)
         }, 2000);
     } else {
+        body.classList.toggle("body-onload");
         setTimeout(() => {
             load.classList.toggle("load-anim");
             setTimeout(() => {
                 load.style.display = "none";
+                body.classList.toggle("body-onload");
             }, 300)
         }, 2000);
     }
